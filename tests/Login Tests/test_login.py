6b928c06d2df9a2edca3975c19e95806e6b8caa9
Alexander Okulov <alexander.okulov@bimeister.com>
2024-12-07 19:19:33 +0300

    Добавил негативный тест

--- tests/Login Tests/test_login.py
+++ tests/Login Tests/test_login.py
@@ -1,4 +1,3 @@
-import time
 import pytest
 from selenium.webdriver.common.by import By
 from tests.pages.login_page import LoginPage
@@ -7,11 +6,24 @@ from tests.pages.login_page import LoginPage
 @pytest.mark.login
 def test_valid_login(driver):
     login_page = LoginPage(driver)
-    login_page.login()
+    login_page.login(LoginPage.valid_username, LoginPage.valid_password)
     actual_username = driver.find_element(By.XPATH, "//*[@id='pt-userpage']/a/span").text.lower()
+    # actual_username = "thetestuser13934923"
 
-    # time.sleep(5)
-    assert LoginPage.username in actual_username, f"Expected username '{LoginPage.username}' to be in '{actual_username}'"
-    print(f"Проверка что введеное имя пользователя: {LoginPage.username}\nсовпадает с указанным на сайте после входа: {actual_username}")
+    assert LoginPage.valid_username in actual_username, f"Expected username '{LoginPage.valid_username}' to be in '{actual_username}'"
+    print(f"Проверка что введеное имя пользователя: {LoginPage.valid_username}\nсовпадает с указанным на сайте после входа: {actual_username}")
+
+def test_invalid_login(driver):
+    login_page = LoginPage(driver)
+    login_page.login(LoginPage.invalid_username, LoginPage.invalid_password)
+
+    # error_message = driver.find_element(LoginPage.error_message)
+    error_message = login_page.get_error_message()
+    print(f"\nНаходим сообщение об ошибке:\n{error_message}")
+    expected_error_1 = "Введены неверные имя участника или пароль. Попробуйте ещё раз."
+    expected_error_2 = "Возникли проблемы с отправленными данными"
+
+    assert error_message in expected_error_1 or error_message in expected_error_2
+    print(f"\nПроверяем, что сообщение об ошибке совпадает с ожидаемой:\n{expected_error_1}\nили\n{expected_error_2}")
 
 
--- tests/pages/login_page.py
+++ tests/pages/login_page.py
@@ -7,31 +7,35 @@ class LoginPage(BasePage):
     username_input = (By.ID, "wpName1")
     password_input = (By.ID, "wpPassword1")
     login_button = (By.ID, "pt-login")
-    error_message = (By.CSS_SELECTOR, ".error-message")
+    error_message = (By.XPATH, "//*[@class='cdx-message__content']")
     submit_button = (By.ID, 'wpLoginAttempt')
     # submit_button = (By.XPATH, "//*[@id='wpLoginAttempt']")
     username_element = (By.ID, "pt-userpage")
 
     with open(r'C:\Users\Alexandr\PycharmProjects\edu_selenium\src\users.txt', "r") as file:
         line = file.readline().strip()
+        valid_username, valid_password = line.split(",")    # ctrl + D дублирует строку или выделенную область; ctrl + / комментирует строку
 
-        username, password = line.split(",")    # ctrl + D дублирует строку или выделенную область; ctrl + / комментирует строку
+    with open(r'C:\Users\Alexandr\PycharmProjects\edu_selenium\src\negative_users.txt', 'r') as neg_file:
+        n_line = neg_file.readline().strip()
+        invalid_username, invalid_password = n_line.split(",")
 
     main_page_url = "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0"
+    negative_url = "https://ru.wiipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0"
 
     def open_url(self):
         self.driver.get(self.main_page_url)
 
 
-    def login(self):
+    def login(self, login, password):
         self.open_url()
         print(f"Открывается страница: {self.main_page_url}\n")
 
         self.driver.find_element(*self.login_button).click()
         print("Клик на кнопку войти")
-        self.driver.find_element(*self.username_input).send_keys(self.username)
-        self.driver.find_element(*self.password_input).send_keys(self.password)
-        print(f"Ввод логина и пароля:\n{self.username}\n{self.password}")
+        self.driver.find_element(*self.username_input).send_keys(login)
+        self.driver.find_element(*self.password_input).send_keys(password)
+        print(f"Ввод логина и пароля:\n{login}\n{password}")
         self.driver.find_element(*self.submit_button).click()
         print("Нажатие кнопки войти после ввода данных")
 
